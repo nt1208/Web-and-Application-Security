d94f0ce93ff1c2ef60154fb9b8717acabc13bce2
nt1208 <hongocthien2k3@gmail.com>
2024-06-29 00:00:46 +0700

    Auto mail

--- Final Project/Auto_mail.py
+++ Final Project/Auto_mail.py
@@ -16,14 +16,14 @@ driver.maximize_window()
 
 driver.get("https://accounts.google.com/v3/signin/identifier?ifkv=AaSxoQxb9tcMaa03oR2dqoaFnE_k4er1TnnF6FSedFpR_U7kR-Itp2RmMObFToj_9_yG-4S3IdOAQw&lp=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1308527356%3A1716776840944936&ddm=0")
 mail_field = driver.find_element(By.NAME,"identifier")
-mail_field.send_keys("hongocthien2k3@gmail.com")
+mail_field.send_keys("abc@gmail.com")
 mail_field.send_keys(Keys.RETURN)
 
 driver.implicitly_wait(2)
 password_field = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.NAME, "Passwd"))
             )
-password_field.send_keys("Ngocthien@1208")
+password_field.send_keys("abc")
 password_field.send_keys(Keys.RETURN)
 
 compose_btn = WebDriverWait(driver, 15).until(
@@ -36,7 +36,7 @@ driver.implicitly_wait(5)
 recipient_field = driver.recipient_field = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR,'input[class="agP aFw"]'))
             )
-recipient_field.send_keys("hongocthien2k3@gmail.com")
+recipient_field.send_keys("heo2097@gmail.com")
 recipient_field.send_keys(Keys.RETURN)
 
 content_field = driver.find_element(By.CSS_SELECTOR,'div[class="Am aiL Al editable LW-avf tS-tW"]')
---
+++ Final Project/README.md
@@ -0,0 +1,2 @@
+## Selenium for Automated Testing
+In this project, we used Python with Selenium for scanning 4 vulnerabilities(XSS, SQL Injection, Brute-force password, Path traversal) 
